8f6992c431bb0d3c28e7c48862ec77aa31a2ebdb
Will Tesler <willtesler@gmail.com>
2022-04-01 14:37:33 -0400

    Bump

--- src/test-env/src/Components/Root/Root.js
+++ src/test-env/src/Components/Root/Root.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './Root.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import FirebaseStartup from "./Firebase/FirebaseStartup";
 import serverConfig from "../../Config/SERVER_CONFIG.json";
 import RolesScreen from "../../../../lib/index";
+
 /**
  * Top-Level Component for the App.
  */
 const Root = () => {
   const path = '/roles';
 
+  const withSuspense = (component) => {
+    return (
+      <Suspense fallback={null}>
+        {component}
+      </Suspense>
+    );
+  }
+
   return (
     <div id='Root'>
       <FirebaseStartup/>
       <BrowserRouter>
         <Routes>
-          <Route path={path}><RolesScreen host={serverConfig.host}/></Route>
-          <Route>{`Go to ${path}`}</Route>
+          <Route path={path} element={withSuspense(<RolesScreen host={serverConfig.host}/>)}/>
         </Routes>
       </BrowserRouter>
     </div>
